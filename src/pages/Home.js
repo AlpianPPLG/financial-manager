c0641c9e3dfc426c4864ac1e419f3ff9b8ea3d1c
Alpian <Nova07pplg@gmail.com>
2024-07-08 21:16:52 +0800

    initial comm9t

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Container, Button } from "react-bootstrap";
 import "../styles/Home.css";
 
 const Home = () => {
@@ -7,6 +7,7 @@ const Home = () => {
     <Container className="home">
       <h1>Selamat Datang di Financial Manager</h1>
       <p>Kelola keuangan pribadi Anda dengan mudah dan efektif.</p>
+      <Button className="btn-get-started" variant="primary" size="lg">Mulai Sekarang</Button>
     </Container>
   );
 };
